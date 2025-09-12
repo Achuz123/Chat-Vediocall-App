993871c876c69332ebf89cb28a96a0b882a27053
Achuth B <achutampi19@gmail.com>
2025-09-12 20:04:56 +0530

    vedio call

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -87,9 +87,16 @@ const App = () => {
           }
         />
         <Route
-          path="/call"
-          element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
+          path="/call/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <CallPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
         />
+
         <Route
           path="/notifications"
           element={
